557f51b9009ab7320b04e33d11e8e3836795edd7
Malharii <mrmalhari96@gmail.com>
2024-07-30 15:38:47 +0530

    react addmin dashboard set up

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App({ name }) {
+  console.log(name);
+  return (
+    <>
+      <h1 className="text-3xl font-bold  text-red-500">Hello, World!</h1>
+    </>
+  );
+}
+
+export default App;
